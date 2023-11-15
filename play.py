31d2b246fd6eabf09e0ddece88fbdaae3b1f1107
ReemAZitawi <150894213+ReemAZitawi@users.noreply.github.com>
2023-11-15 18:04:24 +0300

    Add files via upload

--- play.py
+++ play.py
@@ -1,43 +1,39 @@
 import random
 
-def choose_random_word():
-    word_list = ["strawberry", "watermelon", "orange", "cucumber", "flower", "pineapple", "papaya", "apple"]
-    return random.choice(word_list)
-
-def display_clues(word):
-    print("\nClues:")
-    print(f"- The word has {len(word)} letters.")
-    print(f"- The first letter is '{word[0]}'.")
-    print(f"- The word ends with '{word[-2:]}'.")
-
-def mystery_word_game():
-    print("Welcome to Mystery Word!")
+def guess_the_number():
+    print("Welcome to the Guess the Number Game!")
 
     while True:
 
-        secret_word = choose_random_word()
-        attempts_left = 3
+        lower_number = int(input("Enter the lower number of the range: "))
+        upper_number = int(input("Enter the upper number of the range: "))
 
 
-        display_clues(secret_word)
+        target_number = random.randint(lower_number, upper_number)
 
 
-        while attempts_left > 0:
-            print(f"\nAttempts left: {attempts_left}")
-            player_guess = input("Guess the word: ").lower()
+        attempts = 0
+        guessed_number = None
 
-            if player_guess == secret_word:
-                print(f"Congratulations! You guessed the word \"{secret_word}\"! Well done!")
-                break
+        print(f"\nGuess the number between {lower_number} and {upper_number}:")
+
+
+        while guessed_number != target_number:
+            guessed_number = int(input())
+            attempts += 1
+
+            if guessed_number < target_number:
+                print("Too low! Try again.")
+            elif guessed_number > target_number:
+                print("Too high! Try again.")
             else:
-                print("Incorrect. Try again with more clues.")
-                attempts_left -= 1
+                print(f"Congratulations! You guessed the correct number ({target_number}) in {attempts} attempts.")
 
 
         play_again = input("\nDo you want to play again? (yes/no): ").lower()
         if play_again != 'yes':
-            print("Thanks for playing Mystery Word! Hope to see you again soon!")
+            print("Thanks for playing! Hope to see you again soon!")
             break
 
 if __name__ == "__main__":
-    mystery_word_game()
+    guess_the_number()
